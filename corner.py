9a3d30867d43632011a25082da5557a2fcd1fa8d
Daniil <danillkov2007@yandex.ru>
2017-11-02 02:55:55 +0300

    great corner

--- corner.py
+++ corner.py
@@ -1,11 +1,13 @@
 import cv2
 import numpy as np
 
-filename = 'cube.png'
+filename = 'cu.jpg'
 img = cv2.imread(filename)
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+imblue = np.float32(gray)
+imblue = cv2.medianBlur(imblue, 3)
 
-dst = cv2.cornerHarris(gray,2,3,0.04)
+dst = cv2.cornerHarris(imblue,4,5,0.01)
 
 #result is dilated for marking the corners, not important
 dst = cv2.dilate(dst,None)
