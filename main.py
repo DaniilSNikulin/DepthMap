ac7b58c47641a6440d2e54ec91c0593f4fbe9b41
Daniil <danillkov2007@yandex.ru>
2017-11-04 20:53:50 +0300

    Presentation version

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys, getopt
 import cv2
 from matplotlib import pyplot as plt
 
@@ -36,27 +37,73 @@ def pretty_show(img, img_edged, img_durty_lines, img_lines, img_grad,
     plt.subplot(232), plt.imshow(img_edged, cmap='gray')
     plt.title('Edged'), plt.xticks([]), plt.yticks([])
     plt.subplot(233), plt.imshow(img_durty_lines, cmap='gray')
-    plt.title('Durty lines'), plt.xticks([]), plt.yticks([])
+    plt.title('Dirty lines'), plt.xticks([]), plt.yticks([])
     plt.subplot(234), plt.imshow(img_lines, cmap='gray')
     plt.title('Clear lines'), plt.xticks([]), plt.yticks([])
     plt.subplot(235), plt.imshow(img_grad, cmap='gray')
     plt.title('Gradient'), plt.xticks([]), plt.yticks([])
     plt.subplot(236), plt.imshow(img_background, cmap='gray')
-    plt.title('Backgroung'), plt.xticks([]), plt.yticks([])
+    plt.title('Depth map'), plt.xticks([]), plt.yticks([])
 
     plt.show()
 
+def help_print():
+    print('main.py is python script for building depth map')
+    print('-i <inputfile>')
+    print('-o <outputfile>')
+    print('-s show all steps')
+    print('-h print with message')
+    print('you can not specify output file. in this case output image will display')
 
 if __name__ == "__main__":
-    origin_img = cv2.imread("./images/cubes.jpg")
+
+    argv = sys.argv[1:]
+    inputfile = ''
+    outputfile = ''
+    need_all_show = False
+    try:
+        opts, args = getopt.getopt(argv,"shi:o:",["ifile=","ofile="])
+    except getopt.GetoptError:
+        print('main.py -i <inputfile> -o <outputfile>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            help_print()
+            sys.exit(0)
+        if opt == '-s':
+            need_all_show = True
+        elif opt in ("-i", "--ifile"):
+            inputfile = arg
+        elif opt in ("-o", "--ofile"):
+            outputfile = arg
+    if inputfile == '':
+        print('Error: you need specify input file')
+        help_print()
+        sys.exit(0)
+
+    origin_img = cv2.imread(inputfile)
+    if origin_img is None:
+        print("Error: can't open image file")
+        exit(0)
+
     cubes = get_cubes(origin_img)
+    if len(cubes) == 0:
+        print("Error: can't find cubes in image")
+        exit(0)
 
     print("n_cubes",len(cubes))
-    cube1 = cubes[1]
+    cube1 = cubes[0]
 
     img_backgrnd= create_floor_grad(cubes, len(origin_img), len(origin_img[0]), cube1.vps)
 
-    pretty_show(origin_img, cube1.img_edged, cube1.img_lines,
-                img_with_lines(cube1.img, cube1.dirty_lines),
-                cube1.img_cntred, img_backgrnd)
+    if outputfile == '':
+        plt.imshow(img_backgrnd, cmap='gray')
+        plt.title('Depth map'), plt.xticks([]), plt.yticks([])
+        plt.show()
+    elif not outputfile is None:
+        cv2.imwrite(outputfile, img_backgrnd)
+
+    if need_all_show:
+        pretty_show(origin_img, cube1.img_edged, img_with_lines(cube1.img, cube1.dirty_lines),
+                    cube1.img_lines, cube1.img_grad_cntred, img_backgrnd)
 
